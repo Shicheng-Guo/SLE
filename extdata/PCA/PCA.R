4466bc4e082244e69c11f5d34ebc5afa89ce940c
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-02-27 23:21:36 -0600

    Create PCA.R

---
+++ extdata/PCA/PCA.R
@@ -0,0 +1,58 @@
+
+PCAPlot<-function(data,pheno,output,multifigure=T){
+  print("Please be sure row is individual, column is variable")
+  nacol<-which(unlist(apply(data,2,function(x) all(is.na(x)))))
+  if(length(nacol)>0){
+    matrix<-matrix[,-narow]
+  }
+  pca <- prcomp(data,center=T,scale = F)  # Here, input file: row is individual and column is variable
+  outputfile=paste(output,".pdf",sep="")
+  pdf(outputfile)
+  if(multifigure){
+    par(mfrow=c(2,2),mar=c(4,4,4,4)) 
+  }
+  plot((pca$sdev[1:10])^2,type="o",xaxt="n",ylab="Variances",xlab="Principle Components",col="red",lwd=2)
+  axis(1,at=0:10,labels=paste("PC",0:10,sep=""))
+  var<-c()
+  for(i in 1:length(pca$sdev)){var[i]<-sum((pca$sdev[1:i])^2)/sum((pca$sdev)^2)}
+  plot(var,ylab="total variance",xlab="number of principle components",lwd=2,type="l")
+  abline(h=0.8,col="grey",lty=2)
+  abline(v=which(var>0.8)[1],col="grey",lty=2)
+  scores <- data.frame(pheno, pca$x[,1:3])
+  col = as.numeric(as.factor(pheno))
+  plot(x=scores$PC1,y=scores$PC2, xlim=c(min(scores$PC1),max(scores$PC1)),ylim=c(min(scores$PC2),max(scores$PC2)),type="n",xlab="PC1",ylab="PC2")
+  for(i in 1:length(scores$PC1)){
+    points(scores$PC1[i],scores$PC2[i],pch=as.numeric(as.factor(pheno))[i],col=col[i],cex=0.8,lwd=2)
+  }
+  legend("topright",legend=names(table(pheno)),pch=1:length(table(pheno)),col=1:length(table(pheno)),bty="n",cex=0.6)
+  plot(x=scores$PC1,y=scores$PC3, xlim=c(min(scores$PC1),max(scores$PC1)),ylim=c(min(scores$PC3),max(scores$PC3)),type="n",xlab="PC1",ylab="PC3")
+  for(i in 1:length(scores$PC1)){
+    points(scores$PC1[i],scores$PC3[i],pch=as.numeric(as.factor(pheno))[i],col=col[i],cex=0.9,lwd=2)
+  }
+  legend("bottomright",legend=names(table(pheno)),pch=1:length(table(pheno)),col=1:length(table(pheno)),bty="n",cex=0.6)
+  dev.off()
+}
+
+
+data<-read.csv("BCN.csv",row.names=1)
+data<-data[,2:ncol(data)]
+
+BM<-unlist(lapply(strsplit(colnames(data),"_"),function(x) x[2]))
+HS<-unlist(lapply(strsplit(colnames(data),"_"),function(x) x[1]))
+
+HS[grep("HC",HS)]<-"HC"
+HS[grep("S",HS)]<-"SLE"
+
+input<-t(data)
+
+BMHS<-paste0(HS,"_",BM,paste="")
+
+PCAPlot(input,BM,output="BM")
+PCAPlot(input,HS,output="HS")
+PCAPlot(input,BMHS,output="BM_HS")
+
+
+
+
+
+
